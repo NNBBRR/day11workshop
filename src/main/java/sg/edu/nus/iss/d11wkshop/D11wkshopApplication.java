d984b2b56fd6fc60c7600cf15b4795f5131cff2e
Ng Bing Rong <ngbingrong@gmail.com>
2022-07-18 13:52:52 +0800

    first

---
+++ src/main/java/sg/edu/nus/iss/d11wkshop/D11wkshopApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.d11wkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D11wkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D11wkshopApplication.class, args);
+	}
+
+}
